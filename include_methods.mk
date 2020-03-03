6b4995b328d05d1f2920cecededa3453e539f6ab
Milan Malfait <milan.malfait94@gmail.com>
2020-03-03 14:20:54 +0100

    Exclude SAMseq method from test run

--- include_methods.mk
+++ include_methods.mk
@@ -3,8 +3,8 @@ empty :=
 space := $(empty) $(empty)
 
 # ## Methods run with R 3.3
-MT3.3 := edgeRLRT SAMseq Wilcoxon#\
-# edgeRQLF NODES NODESnofilt BPSC DESeq2 DESeq2nofilt edgeRLRTdeconv \
+MT3.3 := edgeRLRT Wilcoxon#\
+# SAMseq edgeRQLF NODES NODESnofilt BPSC DESeq2 DESeq2nofilt edgeRLRTdeconv \
 # MASTcpm MASTcpmDetRate MASTtpm MASTtpmDetRate SCDE edgeRLRTrobust voomlimma SeuratBimod \
 # SeuratBimodnofilt SeuratBimodIsExpr2 SeuratTobit DESeq2census edgeRLRTcensus monoclecensus \
 # monocle D3E limmatrend ROTSvoom ROTScpm ROTStpm metagenomeSeq ttest monoclecount \
