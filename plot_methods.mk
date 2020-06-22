adc2e27a94bd3c17631f4041f95b3d851410a3bc
Milan Malfait <milan.malfait94@gmail.com>
2020-06-22 13:28:27 +0200

    Uncomment methods, exclude R 3.4 methods

--- plot_methods.mk
+++ plot_methods.mk
@@ -6,11 +6,11 @@ include include_methods.mk
 
 ## Define methods to include in summary plots
 #MTplot := $(MT)
-MTplot := edgeRLRT Wilcoxon#\
-# edgeRQLF NODES NODESnofilt BPSC DESeq2 DESeq2nofilt edgeRLRTdeconv \
-# MASTcpm MASTcpmDetRate MASTtpm MASTtpmDetRate SCDE edgeRLRTrobust voomlimma SeuratBimod \
-# SeuratBimodnofilt SeuratBimodIsExpr2 SeuratTobit DESeq2census edgeRLRTcensus monoclecensus \
-# monocle D3E limmatrend ROTSvoom ROTScpm ROTStpm metagenomeSeq ttest monoclecount \
-# scDD DEsingle edgeRQLFDetRate DESeq2betapFALSE DESeq2LRT zinbwaveedgeR zinbwaveDESeq2 logregLRT
-# 
+MTplot := edgeRLRT Wilcoxon \
+edgeRQLF NODES NODESnofilt BPSC DESeq2 DESeq2nofilt edgeRLRTdeconv \
+MASTcpm MASTcpmDetRate MASTtpm MASTtpmDetRate SCDE edgeRLRTrobust voomlimma SeuratBimod \
+SeuratBimodnofilt SeuratBimodIsExpr2 SeuratTobit DESeq2census edgeRLRTcensus monoclecensus \
+monocle D3E limmatrend ROTSvoom ROTScpm ROTStpm metagenomeSeq ttest monoclecount \
+edgeRQLFDetRate DESeq2betapFALSE
+
 MTplotc := $(subst $(space),$(comma),$(MTplot))
