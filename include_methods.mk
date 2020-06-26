906f46af16d318a21669c61e220f5b4173adb6fe
Milan Malfait <milan.malfait94@gmail.com>
2020-06-26 15:46:52 +0200

    Exclude D3E (requires Python)

--- include_methods.mk
+++ include_methods.mk
@@ -7,8 +7,9 @@ MT3.3 := edgeRLRT Wilcoxon \
 SAMseq edgeRQLF BPSC DESeq2 DESeq2nofilt edgeRLRTdeconv \
 MASTcpm MASTcpmDetRate MASTtpm MASTtpmDetRate SCDE edgeRLRTrobust voomlimma SeuratBimod \
 SeuratBimodnofilt SeuratBimodIsExpr2 SeuratTobit DESeq2census edgeRLRTcensus monoclecensus \
-monocle D3E limmatrend ROTSvoom ROTScpm ROTStpm metagenomeSeq ttest monoclecount \
+monocle limmatrend ROTSvoom ROTScpm ROTStpm metagenomeSeq ttest monoclecount \
 edgeRQLFDetRate DESeq2betapFALSE
+# D3E  # requires Python support
  
 ## All methods
 MT := $(MT3.3)# $(MT3.4) $(MT3.4bc3.6)
--- plot_methods.mk
+++ plot_methods.mk
@@ -10,7 +10,8 @@ MTplot := edgeRLRT Wilcoxon \
 edgeRQLF BPSC DESeq2 DESeq2nofilt edgeRLRTdeconv \
 MASTcpm MASTcpmDetRate MASTtpm MASTtpmDetRate SCDE edgeRLRTrobust voomlimma SeuratBimod \
 SeuratBimodnofilt SeuratBimodIsExpr2 SeuratTobit DESeq2census edgeRLRTcensus monoclecensus \
-monocle D3E limmatrend ROTSvoom ROTScpm ROTStpm metagenomeSeq ttest monoclecount \
+monocle limmatrend ROTSvoom ROTScpm ROTStpm metagenomeSeq ttest monoclecount \
 edgeRQLFDetRate DESeq2betapFALSE
+# D3E 
 
 MTplotc := $(subst $(space),$(comma),$(MTplot))
